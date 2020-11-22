38002ac148e8918a1dfb30655ba0c1f29c3e34d8
shishir <agrharis1420@gmail.com>
2020-11-22 17:13:57 +0530

    fixed some bugs

--- imgView.py
+++ imgView.py
@@ -5,9 +5,9 @@ init_colorit()
 
 image=Image.open(input("enter the path of image:"))
 
-h=input("enter ht")
-w=input("enter wth")
-image=image.resize((h,w))
+h=int(input("enter ht"))
+w=int(input("enter wth"))
+image=image.resize((h, w))
 
 
 for y in range (image.size[1]):
